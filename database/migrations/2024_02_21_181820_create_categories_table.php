f4b18998956768f4f94a966416284b35580b9647
FujiwaraSumire <smile01183170@gmail.com>
2024-03-12 15:21:38 +0900

    migrationの変更2

--- database/migrations/2024_02_21_181820_create_categories_table.php
+++ database/migrations/2024_02_21_181820_create_categories_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
